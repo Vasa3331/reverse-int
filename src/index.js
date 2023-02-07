6e1027696c2232d13f556e3df7291691812d38bc
vasa3331 <vasiliymartinov@gmail.com>
2023-02-07 13:32:32 +0300

    feat:change Math.metod

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    while (n % 10 == 0) {
+        n = n / 10;
+    }
+    let s = '' + Math.sign(n) * n;
+    let result = '';
+    for (let i = s.length - 1; i >= 0; i--) {
+      result += s[i];
+    }
+    return result;
+  }
+
